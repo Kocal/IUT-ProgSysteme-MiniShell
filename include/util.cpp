cd5e232efab2314f3e52197ee46af55571458a2b
ALLIAUME Hugo <kocal@live.fr>
2015-11-12 23:01:45 +0100

    Correction de l'indentation

--- include/util.cpp
+++ include/util.cpp
@@ -13,17 +13,17 @@ Util::Util() {
 }
 
 string & Util::ltrim(string &s) {
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-        return s;
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
 }
 
 string & Util::rtrim(string &s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-        return s;
+    s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    return s;
 }
 
 string & Util::trim(string &s) {
-        return Util::ltrim(Util::rtrim(s));
+    return Util::ltrim(Util::rtrim(s));
 }
 
  string Util::replaceSpacesBySpace(string s) {
